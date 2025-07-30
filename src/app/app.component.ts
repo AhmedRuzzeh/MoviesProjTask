27261821b23e3de07f8c5dbaf59e937322ef0773
Ahmed Ruzzeh <ahmadm.ruzzeh@gmail.com>
2025-07-30 19:14:46 +0300

    Code02

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { BodyComponent } from './components/body/body.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent, FooterComponent]
+  imports: [HeaderComponent, FooterComponent, BodyComponent]
 })
 export class AppComponent {}
---
+++ src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router'
+
+import { BodyComponent } from './components/body/body.component'
+
+export const routes: Routes = [
+    {
+        path: 'Home',
+        component: BodyComponent,
+    }
+]
