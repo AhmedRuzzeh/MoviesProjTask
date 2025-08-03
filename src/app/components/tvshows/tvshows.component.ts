77b02e425c8b9a69d0438fa1dd1decabcbff2ca3
Ahmed Ruzzeh <ahmadm.ruzzeh@gmail.com>
2025-08-03 14:34:24 +0300

    Code04 -HTML&CSS

--- src/app/components/tvshows/tvshows.component.ts
+++ src/app/components/tvshows/tvshows.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { CardsComponent } from '../../shared/cards/cards.component';
+import { FilterbarComponent } from '../../shared/filterbar/filterbar.component';
 
 @Component({
   selector: 'app-tvshows',
   standalone: true,
-  imports: [],
+  imports: [CardsComponent, FilterbarComponent],
   templateUrl: './tvshows.component.html',
   styleUrl: './tvshows.component.css'
 })
